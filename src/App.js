c2ed99832daf18bd43ab1c0290fe76ad7959472d
Nicholas P Smith <nick@dreamlinestudios.com>
2021-04-28 10:44:30 -0500

    Add login form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LoginForm from "./LoginForm";
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
@@ -14,6 +15,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <LoginForm />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
